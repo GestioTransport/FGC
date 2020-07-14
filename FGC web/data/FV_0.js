038c3a3093de27cfe36d2743898ac9c8f9f89d3d
GestioTransport <63048685+GestioTransport@users.noreply.github.com>
2020-07-14 11:35:50 +0200

    Add files via upload

---
+++ FGC web/data/FV_0.js
@@ -0,0 +1,9 @@
+var json_FV_0 = {
+"type": "FeatureCollection",
+"name": "FV_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "shape_id": "FV_asc", "begin": 0.0, "end": 4.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 2.111263, 41.409328 ], [ 2.10943, 41.410995 ], [ 2.108722, 41.411617 ], [ 2.106553, 41.413525 ], [ 2.105759, 41.414185 ] ] ] } },
+{ "type": "Feature", "properties": { "shape_id": "FV_des", "begin": 0.0, "end": 4.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 2.105759, 41.414185 ], [ 2.106553, 41.413525 ], [ 2.108722, 41.411617 ], [ 2.10943, 41.410995 ], [ 2.111263, 41.409328 ] ] ] } }
+]
+}
